ee59d5af1dfd83255df6d143869ca7b488d529fc
psprimont <107503874+psprimont@users.noreply.github.com>
2022-11-21 14:46:06 -0800

    centered headings based on result

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ var winner1 = "🚩 Player 1 Wins!"; // h1 text for player 1 win
 var winner2 = "Player 2 Wins! 🚩 "; // h1 text for player 2 win
 var draw = "Draw!"; // h1 text for draw
 var winner; // variable for winner of each round
+var style; // variable for margins based on winner
 
 if (randomNumber1 > randomNumber2) {
   winner = winner1;
+  style = "30px 105px 10px 5px";
 } else if (randomNumber1 < randomNumber2) {
   winner = winner2;
+  style = "30px 1px 10px 90px";
 } else {
   winner = draw;
 };
 
 document.querySelector("h1").textContent = winner; // declare winner of dice roll in heading
+document.querySelector("h1").style.margin = style; // set margins according to winner
